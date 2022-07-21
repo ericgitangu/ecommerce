ec3d1e05e507bde9349cfa91ec4fa383a8cf1bf4
Eric <Eric.Gitangu@rgare.com>
2022-07-21 00:56:13 -0500

    Finish checkout -stripe 401.

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Link from 'next/link';
-import { AiOutlineShopping } from 'react-icons/ai'
+import { AiOutlineShopping, AiOutlineHome } from 'react-icons/ai'
 
 import { Cart } from './';
 import { useStateContext} from '../context/StateContext';
@@ -11,10 +11,11 @@ const Navbar = () => {
   return (
     <div className="navbar-container">
       <p className="logo">
-        <Link href="/">JSM Headphones</Link>
+        <AiOutlineHome/>
+        <Link href="/"> eGitangu Audio</Link>
       </p>
 
-      <button type="button" className="cart-icon" onClick={() => setShowCart(true)}>
+      <button type="button" className="cart-icon" onClick={() => setShowCart(true)} >
         <AiOutlineShopping />
         <span className="cart-item-qty">{totalQuantities}</span>
       </button>
--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,18 @@
-import React from 'react'
-import { Layout } from '../components'
-import '../styles/globals.css'
+import React from 'react';
+import { Toaster } from 'react-hot-toast';
+
+import { Layout } from '../components';
+import '../styles/globals.css';
+import { StateContext } from '../context/StateContext';
 
 function MyApp({ Component, pageProps }) {
-  return( 
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+  return (
+    <StateContext>
+      <Layout>
+        <Toaster />
+        <Component {...pageProps} />
+      </Layout>
+    </StateContext>
   )
 }
 
