e8f0cdc9ed2161fac1737e91dda37ce59173273d
Eric <developer.ericgitangu@gmail.com>
2022-07-23 18:36:45 -0400

    Axios instead of fetch and debug CORS issue with MPESA API - Fix Vecel congig.

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,29 +1,5 @@
-import Cors from 'cors'
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-// Initializing the cors middleware
-const cors = Cors({
-  methods: ['POST', 'HEAD'],
-})
-
-// Helper method to wait for a middleware to execute before continuing
-// And to throw an error when an error happens in a middleware
-function runMiddleware(req, res, fn) {
-  return new Promise((resolve, reject) => {
-    fn(req, res, (result) => {
-      if (result instanceof Error) {
-        return reject(result)
-      }
-      return resolve(result)
-    })
-  })
+export default function handler(req, res) {
+  res.status(200).json({ name: 'John Doe' })
 }
-
-async function handler(req, res) {
-  // Run the middleware
-  await runMiddleware(req, res, cors)
-
-  // Rest of the API logic
-  res.json({ message: 'Hello From NextJS!' })
-}
-
-export default handler
